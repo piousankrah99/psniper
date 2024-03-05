988f08b50985d1e1a940e333b9963a7a316a811f
piousankrah99 <133825879+piousankrah99@users.noreply.github.com>
2024-03-05 14:45:36 +0000

    Update SniperController.java

--- psniper-main/primesniper/src/main/java/primesniper/SniperController.java
+++ psniper-main/primesniper/src/main/java/primesniper/SniperController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
-
 @Controller
 @AllArgsConstructor
 @RequestMapping(path = "/api/v1/primeSniper/")
